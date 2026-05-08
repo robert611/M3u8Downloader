6c72ae9ed6df77448ef93af4a49016fbbbcf8edc
robert611 <cyrkiel86@interia.pl>
2026-05-08 22:04:08 +0200

    Add input and button for m3u8 video link

--- app/main.py
+++ app/main.py
@@ -1,4 +1,16 @@
-from PySide6.QtWidgets import QApplication, QWidget, QVBoxLayout, QLabel
+from PySide6.QtWidgets import (
+    QApplication,
+    QWidget,
+    QVBoxLayout,
+    QHBoxLayout,
+    QLabel,
+    QLineEdit,
+    QPushButton,
+)
+
+def on_download_clicked():
+    print("Kliknięto przycisk")
+    pass
 
 def main():
     app = QApplication([])
@@ -9,7 +21,20 @@ def main():
 
     layout = QVBoxLayout()
 
+    top_layout = QHBoxLayout()
+
+    url_input = QLineEdit()
+    url_input.setPlaceholderText("Wklej URL do m3u8...")
+
+    download_button = QPushButton("Pobierz")
+    download_button.clicked.connect(on_download_clicked)
+
+    top_layout.addWidget(url_input)
+    top_layout.addWidget(download_button)
+
     label = QLabel("M3U8 Downloader")
+
+    layout.addLayout(top_layout)
     layout.addWidget(label)
 
     window.setLayout(layout)
